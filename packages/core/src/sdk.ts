65216425177028cdc566d932fd0039362f79661a
lpfloyd <jeevan.pillay8@gmail.com>
2019-12-13 17:28:11 +1100

    Lerna now references @floyd repository objects

---
+++ packages/core/src/sdk.ts
@@ -0,0 +1,64 @@
+import { TypeDLT, TypeSDK } from "@floyd/utils";
+import { AbstractDLT } from "@floyd/abstract";
+
+/**
+ * This class is the base class the software development kit. It facilitates the methods
+ * that can be carried out using this kit. It's a library that allows developers to create
+ * transactions on different blockchains, using a single interface.
+ *
+ * @author Jeevan Pillay
+ */
+class FloydSDK {
+  /**
+   * The object storing the DLTs loaded by the Overledger sdk
+   */
+  dlts: { [key: string]: AbstractDLT } = {};
+
+  /**
+   * Creates the required configurations for the DLT's provided.
+   * @param {TypeSDK} options
+   */
+  constructor(options: TypeSDK) {
+    // validate
+    this.validateDLT(options.dlts);
+
+    // create dlts
+    options.dlts.forEach((dltConfig: TypeDLT) => {
+      const dlt = this.loadDLT(dltConfig);
+      this.dlts[dlt.name] = dlt;
+    });
+  }
+
+  /**
+   * Validate the provided dlts options
+   * @param dlts
+   */
+  private validateDLT(dlts: TypeDLT[]): void {
+    if (!dlts || dlts.length === 0) {
+      throw new Error("[DLT] There arent any DLTs provided.");
+    }
+  }
+
+  /**
+   * Load the dlt to the Overledger SDK
+   * @param {Object} config
+   * @return { AbstractDLT }
+   */
+  private loadDLT(dltConfig: TypeDLT): AbstractDLT {
+    const dltName = `${dltConfig.name}`;
+    try {
+      const dlt = require(`@floyd/${dltName}`).default;
+      return new dlt(this, dltConfig);
+    } catch (e) {
+      if (e.code === "MODULE_NOT_FOUND") {
+        throw new Error(
+          `[DLT] The DLT name provided is not valid, please add ${dltName} manually`
+        );
+      } else {
+        throw e;
+      }
+    }
+  }
+}
+
+export default FloydSDK;
---
+++ packages/core/test/sdk.spec.ts
@@ -0,0 +1,115 @@
+import FloydSDK from "../src/sdk";
+
+describe("core", () => {
+  describe("dlts", () => {
+    test("should throw error if dlts is empty", () => {
+      let options = {
+        dlts: []
+      };
+      try {
+        new FloydSDK(options);
+      } catch (e) {
+        expect(e).toEqual(Error("[DLT] There arent any DLTs provided."));
+      }
+    });
+
+    test("should throw error if wrong dlt name", () => {
+      let options = {
+        dlts: [{ name: "wrongdlt", provider: { uri: "" } }]
+      };
+
+      try {
+        new FloydSDK(options);
+      } catch (e) {
+        expect(e).toEqual(
+          Error(
+            "[DLT] The DLT name provided is not valid, please add wrongdlt manually"
+          )
+        );
+      }
+    });
+    test("should be able to instantiate multiple dlts", () => {
+      let options = {
+        dlts: [
+          {
+            name: "vechain",
+            provider: {
+              uri: "http://localhost:4444"
+            }
+          },
+          {
+            name: "ethereum",
+            provider: {
+              uri: "http://localhost:4444"
+            }
+          }
+        ]
+      };
+
+      var sdk = new FloydSDK(options);
+      expect(sdk.dlts.vechain).toBeDefined();
+      expect(sdk.dlts.ethereum).toBeDefined();
+
+      expect(sdk.dlts.vechain.provider).toBeDefined();
+      expect(sdk.dlts.ethereum.provider).toBeDefined();
+    });
+
+    test("should fail if one of the dlts is wrong", () => {
+      let options = {
+        dlts: [
+          {
+            name: "vechain",
+            provider: {
+              uri: "http://localhost:4444"
+            }
+          },
+          {
+            name: "wrongdlt",
+            provider: {
+              uri: "http://localhost:4444"
+            }
+          },
+          {
+            name: "ethereum",
+            provider: {
+              uri: "http://localhost:4444"
+            }
+          }
+        ]
+      };
+
+      try {
+        new FloydSDK(options);
+      } catch (e) {
+        expect(e).toEqual(
+          Error(
+            "[DLT] The DLT name provided is not valid, please add wrongdlt manually"
+          )
+        );
+      }
+    });
+
+    test("should fail if uri not provided with dlt", () => {
+      let options = {
+        dlts: [
+          {
+            name: "vechain",
+            provider: {
+              uri: ""
+            }
+          }
+        ]
+      };
+
+      try {
+        new FloydSDK(options);
+      } catch (e) {
+        expect(e).toEqual(
+          URIError(
+            "[Provider]-[uri:] The URI provided for this DLT is not valid"
+          )
+        );
+      }
+    });
+  });
+});
